e6f2fb61436b660ae56a44094183580305112aa0
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2024-02-05 17:51:03 +0200

    Solution to 06. Hot Potato

--- Java Advanced/01. Stacks and Queues/Hot Potato/src/Main.java
+++ Java Advanced/01. Stacks and Queues/Hot Potato/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split("\\s+");
+        ArrayDeque<String> queue = new ArrayDeque<>();
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (String child : input) {
+            queue.offer(child);
+        }
+
+        while (queue.size() > 1) {
+            for (int i = 0; i < n - 1; i++) {
+                queue.offer(queue.poll());
+            }
+
+            System.out.println("Removed " + queue.poll());
+        }
+
+        System.out.println("Last is " + queue.poll());
     }
 }
