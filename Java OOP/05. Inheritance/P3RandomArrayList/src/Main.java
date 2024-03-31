802669e974d4bad0684e5690e2822e4223c6b3b6
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2024-03-31 15:57:29 +0300

    Solutions to Lab-Inheritance

---
+++ Java OOP/05. Inheritance/P3RandomArrayList/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        RandomArrayList randomArrayList = new RandomArrayList();
+        randomArrayList.getRandomElement();
+    }
+}
