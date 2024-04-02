ba7b7cc7b94b52a1253d0bced8550989badc5b3f
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2024-04-02 16:02:02 +0300

    Solutions to 05 and 06 from Inheritance - Exercises

---
+++ Java OOP/06.Exercises Inheritance/P5Restaurant/src/restaurant/Food/MainDish.java
@@ -0,0 +1,11 @@
+package restaurant.Food;
+
+import restaurant.Food.Food;
+
+import java.math.BigDecimal;
+
+public class MainDish extends Food {
+    public MainDish(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
+}
---
+++ Java OOP/06.Exercises Inheritance/P5Restaurant/src/restaurant/beverages/Tea.java
@@ -0,0 +1,11 @@
+package restaurant.beverages;
+
+import restaurant.beverages.HotBeverage;
+
+import java.math.BigDecimal;
+
+public class Tea extends HotBeverage {
+    public Tea(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
