1c23e5df92d7d70fba5f8a70b9b871958b0d254d
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2024-06-07 19:47:02 +0300

    Updating needed layers

---
+++ Spring Fundamentals/mobilelele/src/main/java/bg/softuni/mobilelele/model/UserRegistrationDTO.java
@@ -0,0 +1,5 @@
+package bg.softuni.mobilelele.model;
+
+public class UserRegistrationDTO {
+
+}
---
+++ Spring Fundamentals/mobilelele/src/main/java/bg/softuni/mobilelele/service/UserService.java
@@ -0,0 +1,7 @@
+package bg.softuni.mobilelele.service;
+
+import bg.softuni.mobilelele.model.UserRegistrationDTO;
+
+public interface UserService {
+    void registerUser(UserRegistrationDTO userRegistrationDTO);
+}
